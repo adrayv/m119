188a108d890188b95aa5c4c7c76deee7fba66bfe
adrayv <destin.raymundo@gmail.com>
2018-06-11 02:39:39 +0000

    it works!

--- hw3_p2_pi.py
+++ hw3_p2_pi.py
@@ -2,6 +2,60 @@ from bluepy import btle
 from bluepy.btle import Scanner, DefaultDelegate, ScanEntry, Peripheral
 import struct
 
+import paho.mqtt.client as mqtt
+import time
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("Connected with result code "+str(rc))
+    else:
+        print("Bad Connection. Returned Code=",rc)
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+
+file = open("mac.txt","r")
+path = 'ecem119/2018s/hexiwear/'
+one = path + file.readline().rstrip('\n')
+two = path + file.readline().rstrip('\n')
+file.close()
+
+topic1=one
+topic2=two
+broker="broker.hivemq.com"
+client = mqtt.Client("RaspberryM119 Pi1")
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    #print(msg.topic+" "+str(msg.payload))
+    print(str(msg.payload))
+
+def on_disconnect(client,userdata,flags,rc=0):
+    print("Disconnectred result code "+str(rc))
+    
+def button_publish(button, mac):
+    if button==0:
+        motion="right"
+        messageSent="event "+motion+" @ "+ mac
+        client.publish(topic1,messageSent)
+    if button==1:
+        motion="left"
+        messageSent="event "+motion+" @ "+ mac
+        client.publish(topic1,messageSent)
+    if button==2:
+        motion="up"
+        messageSent="event "+motion+" @ "+ mac
+        client.publish(topic1,messageSent)    
+    if button==3:
+        motion="down"
+        messageSent="event "+motion+" @ "+ mac
+        client.publish(topic1,messageSent)
+
+client.on_connect = on_connect
+client.on_disconnect=on_disconnect
+client.on_message = on_message
+client.connect(broker, 1883, 60)
+
+hexi_addr = None
+
 # This is a delegate for receiving BTLE events
 class BTEventHandler(DefaultDelegate):
     def __init__(self):
@@ -21,6 +75,7 @@ class BTEventHandler(DefaultDelegate):
         if cHandle == 40:
             buttonPressed = struct.unpack('B', data)[0]
             print buttonPressed
+            button_publish(buttonPressed,hexi_addr)
             
 handler = BTEventHandler()
 
